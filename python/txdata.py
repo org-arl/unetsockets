13129448f068b823d09497e61801d14b718b510e
Manu Ignatius <manuignatius@users.noreply.github.com>
2025-08-05 21:07:24 +0100

    Update txdata.py

--- python/txdata.py
+++ python/txdata.py
@@ -17,10 +17,10 @@ node_address = 0
 data = [1,2,3,4,5,6,7]
 
 if (len(sys.argv) < 3):
-	print("Usage : txdata <ip_address> <rx_node_address> <port> \n"
+	print("Usage : txdata <ip_address> <rx_node_address> [port] \n"
 	      "ip_address: IP address of the transmitter modem. \n"
 	      "rx_node_address: Node address of the receiver modem. \n"
-	      "port: port number of transmitter modem. \n"
+	      "port: (optional) port number of transmitter modem (default: 1100). \n"
 	      "A usage example: \n"
 	      "txdata.py 192.168.1.20 5 1100\n");
 	sys.exit();
