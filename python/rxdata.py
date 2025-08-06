c1a46237c5e0630180133b3efb610ba87ef7b70a
Chinmay Pendharkar <553140+notthetup@users.noreply.github.com>
2025-08-06 15:47:19 +0800

    Revert "Clean up unet.py examples"

--- python/rxdata.py
+++ python/rxdata.py
@@ -1,46 +1,57 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# rxdata.py - Receive data using UnetSocket
+#///////////////////////////////////////////////////////////////////////////////
+#//
+#// Script to transmit data.
+#//
+#// In terminal window (an example):
+#//
+#// $ rxdata.py <ip_address> [port]
+#//
+#////////////////////////////////////////////////////////////////////////////////
 
 from unetpy import *
 import sys
-import argparse
 
-ip = 'localhost'
 port = 1100
+ip_address = 'localhost'
 
-parser = argparse.ArgumentParser(description='Receive data using UnetSocket.')
-parser.add_argument('ip', help='IP address of the receiver modem')
-parser.add_argument('port', nargs='?', type=int, default=1100, help='Port number of receiver modem (default: 1100)')
-args = parser.parse_args()
+if (len(sys.argv) < 2):
+	print("Usage : rxdata <ip_address> <rx_node_address> <port> \n"
+	      "ip_address: IP address of the transmitter modem. \n"
+	      "port: port number of transmitter modem. \n"
+	      "A usage example: \n"
+	      "rxdata.py 192.168.1.20 1100\n");
+	sys.exit();
 
-ip = args.ip
-port = args.port
+ip_address = sys.argv[1]
 
-# Connect to the Unet instance
-print(f"Connecting to {ip}:{port}")
-sock = UnetSocket(ip, port)
+if (len(sys.argv) > 2):
+	port = int(sys.argv[2])
+
+print("Connecting to " + ip_address + ":" + str(port));
+sock = UnetSocket(ip_address, port)
 if ( sock == None ):
-	print(f"Couldn't open UnetSocket to {ip}:{port}");
+	print("Couldn't open unet socket");
 	sys.exit();
 
 # Bind to protocol DATA
 if (not sock.bind(Protocol.DATA)):
-	print(f"Couldn't bind the UnetSocket to protocol #{Protocol.DATA}");
+	print("Couldn't open bind to socket");
 	sys.exit();
 
 # Set a timeout of 10 seconds
-sock.setTimeout(10000)
+sock.setTimeout(10000);
 
 # Receive and display data
-print("Waiting for a Datagram...");
+print("Waiting for a Datagram");
 
 ntf = sock.receive()
 
 if (ntf != None):
 	print(ntf)
 else:
-	print("No Datagram received!");
+	print("Error receiving data")
 
-# Close the UnetSocket
+# Close the unet socket
 sock.close()
+
+print("Reception Complete");
--- python/txdata.py
+++ python/txdata.py
@@ -1,42 +1,47 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# txdata.py - Transmit data using UnetSocket
+#///////////////////////////////////////////////////////////////////////////////
+#//
+#// Script to transmit data.
+#//
+#// In terminal window (an example):
+#//
+#// $ txdata.py <ip_address> <rx_node_address> [port]
+#//
+#////////////////////////////////////////////////////////////////////////////////
 
 from unetpy import *
 import sys
-import argparse
 
-ip = 'localhost'
 port = 1100
-address = 0
+ip_address = 'localhost'
+node_address = 0
 data = [1,2,3,4,5,6,7]
 
-parser = argparse.ArgumentParser(
-	description="Transmit data using UnetSocket.",
-	epilog="Example: txdata.py 192.168.1.20 5 1100"
-)
-parser.add_argument("ip", help="IP address of the transmitter modem")
-parser.add_argument("node_address", type=int, help="Node address of the receiver modem (use 0 for broadcast)")
-parser.add_argument("port", nargs="?", type=int, default=1100, help="Port number of transmitter modem (default: 1100)")
+if (len(sys.argv) < 3):
+	print("Usage : txdata <ip_address> <rx_node_address> <port> \n"
+	      "ip_address: IP address of the transmitter modem. \n"
+	      "rx_node_address: Node address of the receiver modem. \n"
+	      "port: port number of transmitter modem. \n"
+	      "A usage example: \n"
+	      "txdata.py 192.168.1.20 5 1100\n");
+	sys.exit();
 
-args = parser.parse_args()
+ip_address = sys.argv[1]
+node_address = sys.argv[2]
 
-ip = args.ip
-address = args.node_address
-port = args.port
+if (len(sys.argv) > 3):
+	port = int(sys.argv[3])
 
-# Connect to the Unet instance
-print(f"Connecting to {ip}:{port}")
-sock = UnetSocket(ip, port)
+print("Connecting to " + ip_address + ":" + str(port));
+sock = UnetSocket(ip_address, port)
 if ( sock == None ):
-	print(f"Couldn't open UnetSocket to {ip}:{port}")
-	sys.exit()
+	print("Couldn't open unet socket");
+	sys.exit();
 
 # Transmit data
-print(f"Transmitting {len(data)} bytes of data to {address}")
-sock.send(data, address, Protocol.DATA);
+print("Transmitting " + str(len(data)) + " bytes of data to " + str(node_address));
+sock.send(data, node_address, Protocol.DATA);
 
-# Close the UnetSocket
+# Close the unet socket
 sock.close()
 
-print("Transmission Complete")
+print("Transmission Complete");
