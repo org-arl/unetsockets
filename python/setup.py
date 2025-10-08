86e7452fd7e1a246b623adaa75f2dbcf2fbb75fd
Chinmay Pendharkar <chinmay@subnero.com>
2025-10-08 15:44:14 +0800

    fix(unetpy): adding an upper limit to fjagepy version

--- python/setup.py
+++ python/setup.py
@@ -5,9 +5,10 @@ with open('README.md') as f:
 
 setup(
     name='unetpy',
-    version='3.1.2',
+    version='3.1.3',
     description='Unet Python Gateway',
     long_description=readme,
+    long_description_content_type='text/markdown',
     author='Mandar Chitre, Prasad Anjangi',
     author_email='mandar@arl.nus.edu.sg, prasad@subnero.com',
     url='https://github.com/org-arl/unet-contrib/tree/master/unetsocket/python',
@@ -17,10 +18,14 @@ setup(
         'Development Status :: 5 - Production/Stable',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10'
     ],
     packages=find_packages(exclude=('tests', 'docs')),
     install_requires=[
         'numpy>=1.11',
-        'fjagepy>=1.7.2'
+        'fjagepy>=1.7.2,<2'
     ]
 )
